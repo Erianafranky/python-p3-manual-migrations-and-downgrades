c3f2fb5a4520cf5f92dfbab9530f4b9f9a9ba464
DorothyAriana <arianadottie@gmail.com>
2023-06-02 15:43:15 +0300

    completed assignment

---
+++ lib/migrations/versions/2929004b79d3_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 2929004b79d3
+Revises: 791279dd0760
+Create Date: 2023-06-02 15:35:43.278891
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2929004b79d3'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
